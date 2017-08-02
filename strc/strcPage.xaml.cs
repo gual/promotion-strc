a7294754b5624b6a9c42c5c22c90d9bbcf8beee3
Gual Escalante <gual23@gmail.com>
2017-08-02 00:55:00 -0600

    First configuration for carousel, added basic sections

--- strc/strcPage.xaml.cs
+++ strc/strcPage.xaml.cs
@@ -8,14 +8,26 @@ namespace strc
 	{
 		public strcPage()
 		{
-            InitializeComponent();
+			InitializeComponent();
 
 			var collectionOfItems = new ObservableCollection<SfCarouselItem>();
 
-			collectionOfItems.Add(new SfCarouselItem() { ItemContent = new Button() { Text = "ItemContent1", TextColor = Color.White, BackgroundColor = Color.FromHex("#ccd256"), FontSize = 12 } });
-			collectionOfItems.Add(new SfCarouselItem() { ItemContent = new Label() { Text = "ItemContent2", BackgroundColor = Color.FromHex("#836186"), FontSize = 12 } });
+			collectionOfItems.Add(new SfCarouselItem() { ItemContent = new Button() { Image = "schools.jpg", Text = "ItemContent1", TextColor = Color.White, BackgroundColor = Color.FromHex("#ccd256"), FontSize = 12 } });
+			collectionOfItems.Add(new SfCarouselItem() { ItemContent = new Button() { Image = "training.jpg", Text = "ItemContent1", TextColor = Color.White, BackgroundColor = Color.FromHex("#836186"), FontSize = 12 } });
+			collectionOfItems.Add(new SfCarouselItem() { ItemContent = new Button() { Image = "history.jpg", Text = "ItemContent1", TextColor = Color.White, BackgroundColor = Color.FromHex("#ccd256"), FontSize = 12 } });
+			collectionOfItems.Add(new SfCarouselItem() { ItemContent = new Button() { Image = "aliances.jpg", Text = "ItemContent1", TextColor = Color.White, BackgroundColor = Color.FromHex("#836186"), FontSize = 12 } });
 
 			carousel.DataSource = collectionOfItems;
+
+			switch (Device.RuntimePlatform)
+			{
+				case Device.iOS:
+					Padding = new Thickness(0, 20, 0, 0);
+					break;
+				default:
+					Padding = new Thickness(0, 0, 0, 0);
+					break;
+			}
 		}
 	}
 }
